3cf70cf5814da09c8d57767eac0de781b455fc1b
Emerald Seale <sealee@os1.engr.oregonstate.edu>
2020-01-28 22:47:50 -0800

    Made a file in a given dir

--- sealee.buildrooms.c
+++ sealee.buildrooms.c
@@ -7,20 +7,27 @@
 
 int main() {
 	
-	int pid = getpid(); //Generate pid to append to directory name
+	int dirPid = getpid(); //Generate pid to append to directory name
 	char dir[NAME_MAX + 1];
-	char *start = "sealee.rooms.";
+	char *dirStart = "sealee.rooms.";
 
-	snprintf(dir, NAME_MAX + 1, "%s%d", start, pid); //Append pid to directory name
-	//printf("%s\n", dir);
+	snprintf(dir, NAME_MAX + 1, "%s%d", dirStart, dirPid); //Append directory pid to directory name
 
 	struct stat st = {0}; //Make a struct to hold the directory stats
 
 	if (stat(dir, &st) == -1) { //If the directory does not already exist
-		if (mkdir(dir, 0755)) { //Make the directory
-			printf(" Directory created\n");
-			fopen("filename", "w");
-		}
+		(mkdir(dir, 0755)); //Make the directory
+		printf("Directory created\n");
+
+		FILE *file;
+		strncat(dir, "/", NAME_MAX + 1); //Append slash to end of directory name
+		//TODO: Append pid to filename
+		strncat(dir, "filename.txt", NAME_MAX + 1); //Finish fullpath to file by appending filename
+		printf(dir);
+		printf("\n");
+		file = fopen(dir, "w"); //Make the file
+		fclose(file);
+
 	} else {
 		perror("Error making directory\n");
 	}
