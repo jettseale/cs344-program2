4135e40c6236deff7fea8eefa125f451e63bac87
Emerald Seale <sealee@os1.engr.oregonstate.edu>
2020-02-03 20:39:12 -0800

    finds newest directory

--- sealee.adventure.c
+++ sealee.adventure.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <dirent.h>
 
 int main() {
-	printf("Hello world!\n");
+	
+	int newestDirTime = -1;
+	char targetDirPrefix[32] = "sealee.rooms.";
+	char newestDirName[256];
+	memset(newestDirName, '\0', sizeof(newestDirName));
+
+	DIR* dirToCheck;
+	struct dirent* fileInDir;
+	struct stat dirAttributes;
+
+	dirToCheck = opendir(".");
+
+	if (dirToCheck > 0) {
+		while ((fileInDir = readdir(dirToCheck)) != NULL) {
+			if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) {
+				printf("Found the prefix: %s\n", fileInDir->d_name);
+				stat(fileInDir->d_name, &dirAttributes);
+
+				if((int)dirAttributes.st_mtime > newestDirTime) {
+					newestDirTime = (int)dirAttributes.st_mtime;
+					memset(newestDirName, '\0', sizeof(newestDirName));
+					strcpy(newestDirName, fileInDir->d_name);
+					printf("Newer subdir: %s, new time: %d\n",
+						fileInDir->d_name, newestDirTime);
+				}
+			}
+		}
+	}
+	
+	closedir(dirToCheck);
+
+	printf("Newest entry found is: %s\n", newestDirName);
+
 	return 0;
 }
